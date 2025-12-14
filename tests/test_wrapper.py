f26b5a816a2058aec3b39ad59c251ff2d76d293c
Victoria Cheung <victoriakcheung@gmail.com>
2025-12-14 11:31:51 -0800

    update param name

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -8,4 +8,4 @@ from rpy_bridge import RFunctionCaller
 def test_missing_script_raises():
     # If script_path does not exist, the constructor should raise FileNotFoundError
     with pytest.raises(FileNotFoundError):
-        RFunctionCaller(path_to_renv=None, script_path=Path("/does/not/exist.R"))
+        RFunctionCaller(path_to_renv=None, scripts=Path("/does/not/exist.R"))
