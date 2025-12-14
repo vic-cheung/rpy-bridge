30ec064cb468c6cb5cef7ef62cc3d9f011c95893
Victoria Cheung <victoriakcheung@gmail.com>
2025-12-13 22:44:53 -0800

    update rtd files

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,15 @@
+from __future__ import annotations
+
 import os
 import sys
 
 os.environ.setdefault("RPY2_CFFI_MODE", "ABI")
 
-
-# Put the package src on sys.path so autodoc can import it
 sys.path.insert(0, os.path.abspath("../src"))
 
 project = "rpy-bridge"
 author = "Victoria Cheung"
-release = "0.3.2"
+release = "0.3.3"
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -18,17 +18,15 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
-# Avoid importing heavy dependencies (R/rpy2) during doc builds on RTD by mocking
 autodoc_mock_imports = [
     "rpy2",
     "rpy2.robjects",
     "rpy2.rinterface_lib",
     "rpy2.rinterface",
-    "numpy",
-    "pandas",
     "loguru",
 ]
 
+autodoc_typehints = "description"
 autosummary_generate = True
 
 templates_path = ["_templates"]
@@ -37,7 +35,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
-# Show members for classes automatically
 autodoc_default_options = {
     "members": True,
     "undoc-members": True,
